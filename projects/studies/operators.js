5f426a29d6946e046cbedde6dc93a08affdbd5c1
pheeuh <pheeuh@gmail.com>
2022-10-04 03:16:21 +0000

    first commit

--- projects/studies/operators.js
+++ projects/studies/operators.js
@@ -20,7 +20,12 @@
  * resolve these two values as a boolean, as long as BOTH values are true (&&), if at LEAST
  * one of them is true (||), or flips the truthiness of a value (!);
  * 
- * 5: Unary operators act on a single operand to produce a new value (typeof, !, -).
+ * 5: Unary operators act on a single operand to produce a new value (typeof, !, -). The type of operator tells you
+ * what type of data your value is. The bang operator (!) flips the truthiness of your values, while the (-) unary 
+ * minus operator is your negation operator. This is different from your subtraction operator.  
+ * 
+ * 6: Ternary operators take three operands, which follow the format of (condition ? truthy expression : falsy expression)
+ * 
  * 
  * 
  * // 1. assignment operators //
@@ -39,11 +44,14 @@
 
  /**
   * // 3. comparison operators // 
-  * // When comparing two operands, we can use comparison operators, in order to resolve the conditions 
-  * as a boolean (true or false). One condition has to be true and the other has to be false.  
+  * // When comparing two operands, we can use comparison operators (<, >, <=, >=, ==, !==), in order to 
+  * resolve the conditions as a boolean (true or false). One condition has to be true and the other has to be false.  
   */
     var isGreaterThan = (1 > 2);
     console.log(isGreaterThan); // prints => false
+
+    var isLooselyEqual = ("Sophia" == "sophia");
+    console.log(isLooselyEqual); // => prints true
 
 /**
  * // 4. logical operators // 
@@ -57,13 +65,33 @@
     var orOperator = (1 > 2 || 10 > 5);
     console.log(orOperator); // prints => true
 
-    var  bangOperator = (1 != 2);
+    var bangOperator = (1 != 2);
     console.log(bangOperator); // => prints true
-    
+
 /**
  * // 5. unary operators //
- * //
+ * // Unary operators act on a single operand to bring a new value (typeof, !, -). 
  */
+    var typeOf = typeof "Sophia";
+    console.log(typeOf); // => prints string
 
- 
+    var notTrue = (!true);
+    console.log(notTrue); // => prints false
+
+    var x = 3
+    y = -x; 
+    console.log(y); // => prints -3
+
+/**
+ * // 6. ternary operators //
+ * //The only JS operator that takes in 3 operands. A condition followed by a question mark (?), followed by 
+ * an expression that is executed to see if the condition is truthy, followed by a colon (:), and finally the expression
+ * to execute if the condition is falsy. => (condition ? truthy expression : falsy expression)
+ * 
+ * Translation: If the person's age is over 21, they can take wine. If they are not, they take soda!
+ * 
+ */
+    var age = 16; 
+    var beverage = age >= 21 ? "wine" : "soda";
+    console.log(beverage);
  

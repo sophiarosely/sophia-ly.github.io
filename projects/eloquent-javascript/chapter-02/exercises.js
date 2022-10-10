78c6e6331ddc889e7e021afb5f0d771ffc49a29e
pheeuh <pheeuh@gmail.com>
2022-10-10 23:50:39 +0000

    first commit

--- projects/eloquent-javascript/chapter-02/exercises.js
+++ projects/eloquent-javascript/chapter-02/exercises.js
@@ -25,8 +25,22 @@ for numbers divisible by only one of those).
 a significant percentage of programmer candidates. So if you solved it, your
 labor market value just went up.)
  */
-function fizzBuzz() {
-
+function fizzBuzz(num1, num2) {
+  for (var i = num1; i <= num2; i++) {
+    // if parameters are divisible by 3 & 5, prints "fizzbuzz" to console
+    if (i % 15 === 0) {
+      console.log("fizzbuzz");
+      // if parameters are divivisble by 3, prints "fizz" to console
+    } else if (i % 3 === 0) {
+      console.log("fizz");
+      // if parameters are divivisble by 5, prints "buzz" to console
+    } else if (i % 5 === 0) {
+      console.log("buzz");
+      //if parameters are not divisible by the conditions above, their number position prints
+    } else {
+      console.log(i);
+    }
+  }
   }
 
 

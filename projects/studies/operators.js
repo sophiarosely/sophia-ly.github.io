a5934372b57748fdddf16b1ec6b3c5154ff7542b
pheeuh <pheeuh@gmail.com>
2022-10-28 22:28:34 +0000

    first commit

--- projects/studies/operators.js
+++ projects/studies/operators.js
@@ -8,10 +8,10 @@
  * also four categories of operators: arithmetic (binary), comparison (binary),
  * assignment (binary), and logical. 
  * 
- * 1: We need to use the assignment operator (=), in order to initialize variables.
+ * 1: We need to use the assignment operator (=, -=, +=, /=, *=, %=, <<=, >>=), in order to initialize variables. 
  * 
  * 2: Arithmetic operators compute simple math to calculate number values, such as 
- * (+, -, *, /, %).
+ * (+, -, *, /, %, ++, --).
  * 
  * 3: When comparing two values (two operands), you can use comparision operators to (<, >, <=, >=, ==, !==)
  * resolve these two values as a boolean, as long as ONE value is TRUE or ONE value is FALSE.
@@ -29,18 +29,103 @@
  * 
  * 
  * // 1. assignment operators //
- * // When you initalize a variable, you use an assignment operator (=). 
+ *  When you initalize a variable, you can use a simple assignment operator (=), which assigns the value 
+ * to a Javascript variable. However, when you use more complex assignment operators (-=, +=, /=, *=, %=, <<=, >>=)
+ *  it will apply the given arithmetic operator and assign the result to the variable.
  **/
+
+   // simple assignment operator
      var faveColor = "pink"; 
-    console.log(faveColor); // prints => "pink" 
+          console.log(faveColor); // prints => "pink" 
+
+    // addition assignment operator 
+      var sum = 10; 
+         sum += 10;
+            console.log(sum); //=> prints 20 
+
+    // subtraction assignment operator
+      var balance = 200; 
+         balance -= 50; 
+            console.log(balance); // => prints 150
+
+    // multiplication assignment operator 
+      var multiply = 5;
+         multiply *= 5; 
+         console.log(multiply); // => prints 25
+
+    // division assignment operator
+      var divide = 200; 
+         divide /= 10;
+            console.log(divide); // => prints 20
+
+    // remainder assignment operator
+      var remainder = 10;
+         remainder %= 5; 
+            console.log(remainder); // => prints 0 
+
+    // left shift assignment operator
+            var x = 100;
+                  x <<= 2;
+                     console.log(x); // prints 400
+    
+
+    // right shift assignment operator 
+             var y = 100;
+                 y >>= 2;
+                  console.log(x); // => prints 25
+    
+
+    // bitwise and assignment operator
+    var and = 30;
+         and &= 10;
+             console.log(and); // => prints 10 
+
+    // bitwise or assignment operator 
+       let or = 30;
+             or != 10;
+                  console.log(or); // => prints 30
 
  /**
   * // 2. arithmetic operators //
-  * // We can use assignment operators (+, -, *, /, %) to compute math equations with 
+  * // We can use assignment operators (+, -, *, /, %, ++, --) to compute math equations with 
  * number values.
  **/
-    var add = 3 + 3; 
-    console.log(add); // prints => 6 
+         // addition operator 
+         let add = 3 + 3; 
+          console.log(add); // prints => 6 
+
+         // subtraction operator 
+            let deduct = 500 - 250;
+               console.log(deduct); // => prints 250 
+
+         // multiplication operator 
+            let multiplication = 333 * 3; 
+               console.log(multiplication); // => prints 999
+
+         
+         // division operator 
+            let divide = 1000 / 2; 
+               console.log(divide); // => prints 500
+
+         // exponentiation operator 
+         let exponent = 4**2; 
+              console.log(exponent); // => prints 16
+
+         // remainder/modulus operator 
+         let remainder =  20 % 2
+            console.log(remainder); //=> prints 0 
+
+          // increment operator 
+            let value = 2
+            value++
+            let increment = value; 
+                 console.log(increment); //=> prints 3
+    
+            // decrement operator 
+            let value2 = 2
+            value2--
+            let decrement = value2; 
+                 console.log(decrement); //=> prints 1
 
  /**
   * // 3. comparison operators // 
